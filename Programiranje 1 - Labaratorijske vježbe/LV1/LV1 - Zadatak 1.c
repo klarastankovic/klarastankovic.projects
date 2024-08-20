1abbfdb031dcbaec1423cf08dce547d00a31f3a0
Klara Stanković <109075148+klarastankovic@users.noreply.github.com>
2024-08-20 22:09:22 +0200

    org

---
+++ Programiranje 1 - Labaratorijske vježbe/LV1/LV1 - Zadatak 1.c
@@ -0,0 +1,13 @@
+/* U ovoj aktivnosti možete slobodno testirati VPL sustav */
+#include<stdio.h>
+
+int main(void) {
+    short int x;
+    double y;
+    
+    scanf("%hd %lf", &x, &y);
+    
+    printf("%hd %f", x, y);
+    
+    return 0;
+}
---
+++ Programiranje 1 - Labaratorijske vježbe/LV1/LV1 - Zadatak 2.c
@@ -0,0 +1,10 @@
+/* U ovoj aktivnosti možete slobodno testirati VPL sustav */
+#include<stdio.h>
+
+int main(void) {
+    char a='A', b='B';
+    
+    printf("%c %c", a, b);
+    
+    return 0;
+}
