73dadd7e28a1ac91e1e24451fdef4f0a3b71e67b
Klara Stanković <109075148+klarastankovic@users.noreply.github.com>
2025-09-10 19:16:26 +0200

    Delete FERIT directory

--- FERIT/Programiranje 1 - Laboratorijske vježbe/LV1/Priprema/zad10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	char x='a', y='z';
-	
-	printf("%c %c", x, y);
-	
-	return 0;
-}
--- FERIT/Programiranje 1 - Laboratorijske vježbe/LV1/Priprema/zad8.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	double x=123.456, y=0.00789;
-	
-	printf("%e %e", x, y);
-	
-	return 0;
-}
